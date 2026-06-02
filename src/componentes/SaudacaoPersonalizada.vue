09d7d9736d0b3c094034b01698a091c9185c35f1
arthur <engs-arthurkrecskovski@camporeal.edu.br>
2026-06-01 23:26:41 -0300

    terminando trabalho

---
+++ src/componentes/SaudacaoPersonalizada.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  nome: {
+    type: String,
+    default: 'Visitante'
+  }
+})
+</script>
+
+<template>
+  <div class="saudacao-output">
+    Olá, <span>{{ nome }}</span>! Boas-vindas ao Vue 3 🎉
+  </div>
+</template>
